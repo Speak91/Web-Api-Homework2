f921978904f4f6b6f40331e49b3883807907ffb2
Valentin Illarionov <i-valentin@mail.ru>
2022-05-16 20:50:24 +0900

    Логгирование Менеджера метрик в рамках 3 -го задания выполнено

--- MetricsManager/MetricsManager/Controllers/RamMetricsController.cs
+++ MetricsManager/MetricsManager/Controllers/RamMetricsController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,14 +12,22 @@ namespace MetricsManager.Controllers
     [ApiController]
     public class RamMetricsController : ControllerBase
     {
+        private readonly ILogger<RamMetricsController> _logger;
+        public RamMetricsController(ILogger<RamMetricsController> logger)
+        {
+            _logger = logger;
+            _logger.LogDebug(1, "NLog встроен в RamMetricsController");
+        }
         [HttpGet("available/agent/{agentId}")]
         public IActionResult GetAvailableFromAgent([FromRoute] int agentId)
         {
+            _logger.LogInformation($"Метрика по оперативной памяти от агента {agentId} передана");
             return Ok();
         }
         [HttpGet("available/cluster")]
         public IActionResult GetAvailableFromAllCluster()
         {
+            _logger.LogInformation($"Метрика по оперативной памяти от кластера передана");
             return Ok();
         }
     }
